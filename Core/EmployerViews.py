177d96712ff81bac9cd659655779a7a71aecd60d
mich-thy <nmichelle.thy@gmail.com>
2024-05-06 18:04:33 -0700

    added employer profile

---
+++ Core/EmployerViews.py
@@ -0,0 +1,101 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from ResumeAI import settings
+from ResumeAI.Generic.generic_decoraters import employer_required
+from Core.EmployerForms import EmployerProfileForm
+from Core.EmployerModel import EmployerProfile, Job
+from django.db import transaction
+
+
+@login_required
+@employer_required
+def emp_setupProfile(request):
+    if request.method == 'POST':
+        form = EmployerProfileForm(request.POST)
+        if form.is_valid():
+            with transaction.atomic():
+                profile, created = EmployerProfile.objects.get_or_create(user = request.user)
+                profile.company_name = form.cleaned_data['company_name']
+                profile.company_description = form.cleaned_data['company_description']
+                profile.company_website = form.cleaned_data['company_website']
+                profile.contact_email = form.cleaned_data['contact_email']
+                profile.save()
+            return redirect('employer_home')
+    else:
+        form = EmployerProfileForm()
+    return render(request, 'Authorized/Core/Employer/create-employer-profile.html', {
+        'form': form,
+    })
+
+
+# check this later might be wrong
+@login_required()
+@employer_required
+def jobPostingPage(request):
+    job  = get_object_or_404(Job, Job.job_uuid)
+    return render(request, "Authorized/Employer/JobPostings_Employer.html")
+
+@login_required
+@employer_required
+def candidatePage(request):
+    return render(request, "Authorized/Employer/CandidateList.html")
+
+@login_required
+@employer_required
+def profile(request):
+    user = request.user
+    profile = EmployerProfile.objects.get(user=user)
+    jobs = Job.objects.all() 
+    return render(request,"Authorized/Employer/Profile_Employer.html", context={'profile': profile, 'form': form, 'jobs': jobs})
+
+@login_required
+@employer_required
+def edit_profile(request):
+    user = request.user
+    profile = EmployerProfile.objects.get(user=user)
+    if request.method == 'POST':
+        form = EmployerProfileForm(request.POST)
+        if form.is_valid():
+            with transaction.atomic():
+                profile.company_name = form.cleaned_data['company_name']
+                profile.company_description = form.cleaned_data['company_description']
+                profile.company_website = form.cleaned_data['company_website']
+                profile.contact_email = form.cleaned_data['contact_email']
+                profile.save()
+                return redirect('emprofile/')
+        else:
+            form = EmployerProfileForm(form = EmployerProfileForm(instance=profile))
+            return render(request, "Authorized/Employer/edit_employer_profile.html",{'form':form})
+
+@login_required
+@employer_required
+def setup_employer_profile(request):
+    try:
+        profile = EmployerProfile.objects.get(user=request.user)
+    except EmployerProfile.DoesNotExist:
+        profile = None
+
+    if request.method == 'POST':
+        form = EmployerProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            with transaction.atomic():
+                profile = form.save(commit=False)
+                profile.user = request.user
+                profile.save()
+                return redirect('employer_dashboard')
+    else:
+        form = EmployerProfileForm(instance=profile)
+
+    return render(request, 'Authorized/Core/Employer/create-employer-profile.html', context={'form': form})
+                
+# @login_required
+# def home(request):
+#     if not request.user.has_completed_setup:
+#         return redirect('setup')
+
+@login_required
+@employer_required
+def employer_dashboard(request):
+    return render(request, 'Authorized/Core/Employer/dashboard.html')
+# make sure to grab dahsboard.html from karthik's branch
+
--- Core/urls.py
+++ Core/urls.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 from django.urls import path, include
-from Core import NormalViews, views
+from Core import views, EmployerViews, JobSearcherViews
+from Core import gen_views
 
 urlpatterns = [
-    path("", NormalViews.index, name="index"),
-    path("dashboard/", views.home, name="home")
-]
+    path("", views.index, name='index'),
+    path("home/", views.home, name="home"),
+    #dont forget to add job id
+    path("em-job-posting/",EmployerViews.jobPostingPage, name="job_posting_page"),
+    #add job id -- maybe use jinja? 
+    path("em-job-posting-candidatelist/", EmployerViews.candidatePage ,name="candidate_list"),
+    path("emprofile/", EmployerViews.profile, name="employer_profile"),
+    path("edit-emprofile/", EmployerViews.edit_profile, name="edit_profile"),
+    path('emdashboard/', EmployerViews.employer_dashboard, name='employer_dashboard'),
+    path('js-dashboard/', JobSearcherViews.jobsearcher_dashboard, name='jobsearcher_dashboard'),
+    #added as of 12:54pm may 3
+    path('chat/', JobSearcherViews.jobsearcher_chat, name='jobsearcher_chat'),
+    path('settings/',views.settings , name='settings'),
+    path('js-profile/', JobSearcherViews.jobsearcher_profile, name='jobsearcher_profile'),
+    path('js-setup/',JobSearcherViews.js_setup_profile, name='js_setup_profile'),
+    path('download-resume/', gen_views.download_resume, name='download-resume'),
+    path('edit-jsprofile/', JobSearcherViews.edit_profile, name='edit-profile'),
+    # path('edit-job-posting/', EmployerViews.edit_job_posting, name = 'edit_job_posting')
+    path('emp-setup/', EmployerViews.emp_setupProfile, name='emp_setupProfile')
+] 
